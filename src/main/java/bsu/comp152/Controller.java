39248a3a6554e41b602f3f1bf63c5288633db8e2
lkgross <profgross@gmail.com>
2020-07-23 16:55:17 -0400

    Import TextField explicitly, so it doesn't use java.awt instead of javafx

--- src/main/java/bsu/comp152/Controller.java
+++ src/main/java/bsu/comp152/Controller.java
@@ -3,29 +3,26 @@ package bsu.comp152;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.awt.*;
 
 public class Controller {
-/*
+
     @FXML
     private TextField kiloTextField;
-
- */
     @FXML
     private Label resultLabel;
 
     public void handleCalcButton(ActionEvent event) {
         // Get the kilometers.
-        // double kilometers = Double.parseDouble(kiloTextField.getText());
+        double kilometers = Double.parseDouble(kiloTextField.getText());
 
         // Convert the kilometers to miles.
-        // double miles = kilometers * 0.6214;
+        double miles = kilometers * 0.6214;
 
         // Display the results.
-        // resultLabel.setText(String.format("%,.2f miles", miles));
-
-        resultLabel.setText("Equivalence in miles");
+        resultLabel.setText(String.format("%,.2f miles", miles));
     }
 }
 
