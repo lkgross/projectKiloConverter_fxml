8bda1ea4611ce0cc828a2b1f6690e232950ba1c2
lkgross <profgross@gmail.com>
2022-01-06 18:10:27 -0500

    Create Main.fxml in the standalone Scene Builder on my mac

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,33 +1,34 @@
 package bsu.comp152;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 /**
- * Class for creating a window (Stage)
+ * Class for creating a scene for a GUI
  * in a different way from previously
  *
- * The new way is to create the view of the contents of the window
- * in FXML instead of in this Java class as we did before.
+ * The idea is to use drag and drop to put layout containers and controls
+ * in the scene instead of using Java statements.
  *
- * The ML stands for markup language.  HTML is for web pages,
- * and FXML is for JavaFX.
+ * The tool we use is called Scene Builder. (It is a GUI for creating GUIs!)
+ * It's available within IDEA. It is not well-integrated into IDEA on Macs. Mac users
+ * should download the standalone Scene Builder for free at
+ * https://gluonhq.com/products/scene-builder/ .
  *
- * The approach we are about to use will allow us (the programmers)
- * to create the elements of the GUI by dragging and dropping
- * instead of writing FXML code directly. (The contents of
- * web pages can also be created by dragging and dropping instead
- * of writing code.)
+ * When we drag and drop to create a scene visually, Scene Builder
+ * creates a code version of the scene.
+ * The code is in fxml instead of the Java that we wrote before.
+ * The ML in fxml stands for markup language. FXML is for JavaFX.
+ * (HTML is for web pages.)
+ * (The contents of web pages can also be created by dragging and dropping
+ * instead of writing html code directly.)
  *
- * We will need to create an .fxml file as a resource in
+ * We need to put the .fxml file that contains the scene as a resource in
  * the appropriate part of the project structure.
  *
  * To do so, first right click on resources in the project directory.
@@ -41,13 +42,10 @@ import java.io.IOException;
  * It is in the right place because the path
  * main/resources/bsu/comp152 for the FXML resource
  * mirrors the path main/java/bsu/comp152
- * for the Java classes
- * as IDEA expects/requires.
+ * for the Java classes as IDEA expects/requires.
  *
  */
 public class Main extends Application {
-
-    Label myLabel;
 
     public static void main(String[] args) {
         launch(args);
@@ -68,10 +66,11 @@ public class Main extends Application {
 
         // Next we want to load the FXML file from location loc.
 
-        /* We connect the location loc to the file Main.fxml
+        /* We instantiate a location that connects to the file Main.fxml
          * using a "factory constructor" rather than the
          * keyword new:
          */
+        // var loc = getClass().getResource("Main.fxml");
         var loc = getClass().getResource("Main.fxml");
         try {
             /* Overwrite the value of the reference variable
@@ -97,18 +96,22 @@ public class Main extends Application {
     /*
      * And just how will we create this beautiful display in Main.fxml?
      *
-     * Double click on Main.fxml in the project directory to open it.
+     * On a Windows machine, double click on Main.fxml in the project directory
+     * to open it.
      * Toggle to Scene Builder.
      * Build the contents of the window by dragging and dropping a container,
      * followed by other containers and controls as needed.
-     *
-     * It may with visibility and scaling in the drag-and-drop environment
+     * It may help with visibility and scaling in the drag-and-drop environment
      * to collapse the project directory (on the left) and the Gradle buffer
      * (on the right).
      *
-     * You will be able to see the elements you add as a graph
-     * (! Take MATH 130!) at the
-     * bottom of the left edge of Scene Builder.
+     * On a Mac, create the GUI in the standalone Scene Builder.
+     * (Download it for free at https://gluonhq.com/products/scene-builder/ .) In the
+     * File Menu, choose Open and open *your fxml file*. Perhaps it is
+     * IdeaProjects/main/resources/bsu/comp152/Main.fxml .
+     *
+     * You can see the elements you add in a hierarchy at the bottom of the
+     * left edge of Scene Builder.
      *
      * Modify the elements you drop onto the palette by clicking on them in
      * the graph at the bottom of the left edge of Scene Builder and then
@@ -117,11 +120,12 @@ public class Main extends Application {
      * with the element to which they apply, such as a "Button" or a "VBox".
      *
      * (If you get some spurious yellow shapes or shadows for no apparent
-     * reason, then close your project, and reopen it.)
+     * reason, or if the interface doesn't respond, then close your project
+     * and reopen it.)
      *
      * Do you agree that Scene Builder is a convenient way to create a display
      * (a view)? To see what the corresponding .fxml code looks like, which you
-     * have "written" by manipulating Scene Builder, toggle from Scene Builder
+     * have generated by manipulating Scene Builder, toggle from Scene Builder
      * over to Text.
      *
      * If you want your program to respond to user events, such as button clicks,
@@ -131,7 +135,8 @@ public class Main extends Application {
      * How will you proceed?
      * You can write the bodies of event handlers, which we previously
      * wrote in inner classes in Main.java, and write them appropriately
-     * in a Controller, for example as follows (uncommenting the lines below):
+     * in a Controller. For example, you could create a Controller class in
+     * a file Controller.java alongside Main.java. It could contain:
      */
 
     /*
@@ -142,33 +147,26 @@ public class Main extends Application {
     }
     */
 
-    /* However, a catch is for the .fxml file to be able to direct the view
-     * to the controller. (Java has to be able to find the controller when
-     * the user clicks the button.)
-     *
-     * Look at the text file for Main.fxml. Enter
-     * fx:controller="bsu.comp152.Main">
-     *
-     * so that you see something like
-     *
-     * <BorderPane maxHeight="-Infinity" maxWidth="-Infinity" minHeight="-Infinity"
-            minWidth="-Infinity" prefHeight="300.0" prefWidth="300.0"
-            xmlns="http://javafx.com/javafx/10.0.2-internal"
-            xmlns:fx="http://javafx.com/fxml/1" fx:controller="bsu.comp152.Main">
-     *
-     * . Then in the Scene Builder, select the Button from the graph at the bottom
-     * right edge of the window. In the right edge of the window, open
-     * the menu Code for the Button.
-     *
-     * Enter an fx-id, such as button_message .
-     * Select the appropriate option for On Action (like pressButton).
+    /* For the pressButton method above, you need a class variable
+    in the Controller class:
+
+    @FXML
+    private Label myLabel;
      */
 
-    /* Alternatively, you can omit the @FXML and pressButton method from this
-     * file Main.java, and create a new Java file in main/java/bsu/comp152
-     * instead, called for example Controller. In this case, you would need to
-     * look at the text file for Main.fxml and enter
-     * fx:controller="bsu.comp152.Controller">
+    /* For the controller to work, the .fxml file must identify the
+     * file containing the controller.
+     * (Java has to be able to find the controller when the user clicks the button.)
+     *
+     * Use the Controller drop-down in the lower left corner of Scene Builder
+     * under the hierarchy. Enter the file name for the controller in the
+     * Controller-class textfield. Omit the .fxml extension, for example,
+     * bsu.comp152.Controller .
+     *
+     *
+     * Alternatively, connect the view to the controller in the text of the
+     * .fxml file: Look at the text file for Main.fxml. For example, enter
+     * fx:controller="bsu.comp152.Controller"> ,
      *
      * so that you see something like
      *
@@ -176,16 +174,26 @@ public class Main extends Application {
             minWidth="-Infinity" prefHeight="300.0" prefWidth="300.0"
             xmlns="http://javafx.com/javafx/10.0.2-internal"
             xmlns:fx="http://javafx.com/fxml/1" fx:controller="bsu.comp152.Controller">
-      *
-      * See the file Controller.java for the needed contents!
-      *
-      * Using a separate file Controller.java as the controller is preferred
-      * because we'd like to reserve Main for opening windows if multiple
+     *
+     * . Then in the Scene Builder, select the Button. In the right edge of the window, open
+     * the menu Code for the Button.
+     *
+     * Type the method name for the handler in the On Action textfield (here pressButton since
+     * the handler method in the controller is called pressButton).
+     *
+     * In the Code menu for the label, in the fx:id textfield,
+     * enter the reference variable (for example myLabel).
+     */
+
+    /*
+      * The controller code could be included here in Main.java.
+      * Using a separate file Controller.java as the controller is preferred,
+      * however, because we'd like to reserve Main for opening windows if multiple
       * windows will potentially be opened in this program.
       */
 
     /*
-     * Note we are using the the view/contoller organization in the
+     * Note we are using the view/contoller organization in the
      * MVC architcture pattern: model-view-contoller.
      * The view shows you a display.
      * The controller accepts input and converts it to commands.
